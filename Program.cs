6b485a9a24b60751af18cdbb68bc9fbefcc10b3c
Heba Derawi <heba.derawi95@hotmail.com>
2023-10-06 15:16:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Labb5___Grunderna_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Cirkle
+    {
+
+    }
+}
